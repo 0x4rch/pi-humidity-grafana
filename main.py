6ce5fb011513c70a9caa9d04f2f375699dde73e6
Nathan Parker <nparker@thoroughcare.net>
2024-05-09 19:32:30 -0600

    add board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import os
+import board
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from dotenv import load_dotenv
