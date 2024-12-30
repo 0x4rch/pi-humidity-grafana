2b33ca9b62c4bb05a2b55a1e2da5022d417aa58a
0x4rch <njpthepirate@yahoo.com>
2024-12-29 19:30:47 -0700

    move to a module

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 sensor = Sensor(board.D4)
 display = Display()
 temperature_f, humidity = sensor.read_data()
-app = StreamingApp(1280, 720, temperature_f, humidity)
+app = CameraStream(1280, 720, temperature_f, humidity)
 app.start_server()
 while True:
     temperature_f, humidity = sensor.read_data()
